bec6ba3693358a5de5bbeca5c14cc0f7b19654bf
Artūras Danielius <arturas.danielius@gmail.com>
2022-08-11 10:36:17 +0300

    web mechanika1

--- NamuDarbai/nd7webmech1.php
+++ NamuDarbai/nd7webmech1.php
@@ -1,17 +1,36 @@
+<?php
+
+$linkas1 ='Raudona';
+
+$linkas2 = 'Juoda';
+
+
+
+if($_GET['color'] ?? null == 1){
+    $bgColor = 'style="background-color: #000"';
+}else{
+    $bgColor = 'style="background-color: #FF0000"';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Color change</title>
 </head>
-<body style="background-color: #000">
+<body <?= $bgColor ?>>
 <!--1. Sukurti puslapį su juodu fonu ir su dviem linkais (nuorodom) į save. Viena nuoroda su failo vardu, o kita nuoroda su failo vardu ir GET duomenų  perdavimo metodu perduodamu kintamuoju color=1. Padaryti taip, kad paspaudus ant nuorodos su perduodamu kintamuoju fonas nusidažytų raudonai, o paspaudus ant nuorodos be perduodamo kintamojo, vėl pasidarytų juodas.-->
 
-<a href="http://localhost/php/NamuDarbai/nd7webmech1.php">Failo vardas</a>
 
-<a href="http://localhost/php/NamuDarbai/nd7webmech1.php" method="get">xxxxxxx</a>
-    
+
+<a href="http://localhost/php/NamuDarbai/nd7webmech1.php/?color=1"><?= $linkas1 ?></a>
+
+<a href="http://localhost/php/NamuDarbai/nd7webmech1.php/?"><?= $linkas2 ?></a>
+
+
 </body>
 </html>
