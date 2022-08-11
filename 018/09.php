704286e23c10c24a0b08d00b404e5341a4e44333
Artūras Danielius <arturas.danielius@gmail.com>
2022-08-11 16:45:33 +0300

    09 su check box and counter

---
+++ 018/09.php
@@ -0,0 +1,46 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $checked = count($_POST['c'] ?? []);
+    header('Location: http://localhost/php/018/09.php?all='.$checked );
+    die;
+}
+
+$color = isset($_GET['all']) ? 'white' : 'black';
+$aj = range('A', 'J');
+$count = rand(3, 10);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body style="background:<?= $color ?>">
+<?php if(isset($_GET['all'])): ?>
+<a href="http://localhost/php/018/09.php">BACK</a>
+<h1> ALL CHECKED IS : <?= $_GET['all'] ?></h1>
+
+    <?php else: ?>   
+    <div class="two-links">
+        <form action="http://localhost/php/018/09.php" method="post">
+
+            <?php foreach(range(1, $count) as $number): ?>
+            <input type="checkbox" name="c[]" /><?= $aj[$number-1] ?>
+            <?php endforeach ?>
+
+            <button type="submit">GO</button>
+        </form>
+    </div>
+
+<?php endif ?>
+
+</body>
+
+</html>
