95e61a15ed9569d01212a46ea70230fb756d23b2
Artūras Danielius <arturas.danielius@gmail.com>
2022-08-11 16:15:13 +0300

    redirect

---
+++ 018/lemon.php
@@ -0,0 +1,17 @@
+<?php
+header('Location: http://localhost/php/018/orange.php');
+die;
+?>
+<!DOCTYPE html>
+<!-- <meta http-equiv="refresh" content="2; URL='http://localhost/php/018/orange.php'" />  jeigu rasysime htmle, jei php ziureti auksciau   -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background:#fafa33">
+
+</body>
+</html>
