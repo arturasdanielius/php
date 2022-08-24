86e52f9538532a7ddae3aed8d492a8dbd0d0881c
Artūras Danielius <arturas.danielius@gmail.com>
2022-08-24 14:39:51 +0300

    autoload vendor

--- 024/book.php
+++ 024/book.php
@@ -4,48 +4,50 @@
 // require __DIR__ . '/01/Read.php';
 // require __DIR__ . '/02/Read.php';
 
+require __DIR__ . '/vendor/autoload.php';
 
-spl_autoload_register(function ($class) {
-    $prefix = 'Petro\\';
-    $base_dir = __DIR__ . '/01/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
 
-spl_autoload_register(function ($class) {
-    $prefix = 'Antano\\Belekas\\';
-    $base_dir = __DIR__ . '/02/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $prefix = 'Petro\\';
+//     $base_dir = __DIR__ . '/01/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
 
-spl_autoload_register(function ($class) {
-    $prefix = '';
-    $base_dir = __DIR__ . '/01/';
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-    $relative_class = substr($class, $len);
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $prefix = 'Antano\\Belekas\\';
+//     $base_dir = __DIR__ . '/02/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
+
+// spl_autoload_register(function ($class) {
+//     $prefix = '';
+//     $base_dir = __DIR__ . '/01/';
+//     $len = strlen($prefix);
+//     if (strncmp($prefix, $class, $len) !== 0) {
+//         return;
+//     }
+//     $relative_class = substr($class, $len);
+//     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+//     if (file_exists($file)) {
+//         require $file;
+//     }
+// });
 
 
 $b1 = new Petro\Read;
