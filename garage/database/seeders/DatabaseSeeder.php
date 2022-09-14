c3b250a6bcf9e6571a78ecc3b968bfec897974a3
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-14 15:16:55 +0300

    seklintojas

--- garage/database/seeders/DatabaseSeeder.php
+++ garage/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -17,16 +17,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 
-            DB::table('users')->insert([
-                'name' => 'bebras',
-                'email' => 'bebras@bebras.lt',
-                'password' => Hash::make('123'),
-            ]);
-            DB::table('users')->insert([
-                'name' => 'briedis',
-                'email' => 'briedis@briedis.lt',
-                'password' => Hash::make('123'),
-            ]);
-        
+        DB::table('users')->insert([
+            'name' => 'Bebras',
+            'email' => 'bebras@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Briedis',
+            'email' => 'briedis@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
     }
 }
