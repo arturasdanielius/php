3b566aa1babe8bca775d3f39b7d47822a1fdb2bb
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-14 15:24:46 +0300

    index

--- garage/resources/views/mechanic/index.blade.php
+++ garage/resources/views/mechanic/index.blade.php
@@ -6,33 +6,30 @@
         <div class="col-9">
             <div class="card">
                 <div class="card-header">
-                    <h2>Posts</h2>
-                    @foreach($c as $letter)
-                        <h3>{{$letter}}
-                    @endforeach
+                    <h2>Mechanics</h2>
                 </div>
                 <div class="card-body">
                     <ul class="list-group">
-                        @forelse($blogs as $blog)
+                        @forelse($mechanics as $mechanic)
                         <li class="list-group-item">
                             <div class="posts-list">
                                 <div class="content">
-                                    <h2>{{$blog->title}}</h2>
+                                    <h2>{{$mechanic->name}}</h2>
+                                    <h2>{{$mechanic->surname}}</h2>
                                 </div>
                                 <div class="buttons">
-                                    <a href="{{route('show', $blog)}}" class="btn btn-info">Show</a>
-                                   <a href="{{route('edit', $blog)}}" class="btn btn-success">Edit</a>
-                                    <form action="{{route('delete', $blog)}}" method="post">
+                                    <a href="{{route('m_show', $mechanic)}}" class="btn btn-info">Show</a>
+                                    <a href="{{route('m_edit', $mechanic)}}" class="btn btn-success">Edit</a>
+                                    <form action="{{route('m_delete', $mechanic)}}" method="post">
                                     @csrf
                                     @method('delete')
                                     <button type="submit" class="btn btn-danger">Delete</button>
                                     </form>
-                                    
                                 </div>
                             </div>
                         </li>
                         @empty
-                        <li class="list-group-item">No Posts found</li>
+                        <li class="list-group-item">No mechanics found</li>
                         @endforelse
                     </ul>
                 </div>
