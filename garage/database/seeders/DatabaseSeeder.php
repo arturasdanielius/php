e7258fdcf1719ee163be064afbfb46bb032cc7f8
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-15 13:22:52 +0300

    seed

--- garage/database/seeders/DatabaseSeeder.php
+++ garage/database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Faker\Factory as F;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,11 +27,26 @@ class DatabaseSeeder extends Seeder
             'email' => 'briedis@gmail.com',
             'password' => Hash::make('123'),
         ]);
+        $mc = 20;
         $faker = F::create('lt_LT');
-        foreach(range(1, 20) as $_) {
+        foreach(range(1, $mc) as $_) {
             DB::table('mechanics')->insert([
                 'name' => $faker->firstName(),
                 'surname' => $faker->lastName()
+            ]);
+        }
+
+        $makers = ['MB', 'Volvo', 'Scania', 'Kamaz', 'Avia', 'DAF', 'Iveco', 'MAN', 'Ford', 'Mack', 'Tešla'];
+
+
+
+        foreach(range(1, 223) as $_) {
+            DB::table('trucks')->insert([
+                'maker' => $makers[rand(0, count($makers) - 1)],
+                'plate' => strtoupper(Str::random(3)) . '-' . rand(100, 999),
+                'make_year' => rand(1960, 2022),
+                'mechanic_notices' => $faker->paragraph(rand(1, 10)),
+                'mechanic_id' => rand(1, $mc)
             ]);
         }
     }
