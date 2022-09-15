05aa86250ce598b3da6907a0cf1209e988ba2bf6
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-15 13:56:12 +0300

    show

--- garage/app/Models/Mechanic.php
+++ garage/app/Models/Mechanic.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mechanic;
 
 class Mechanic extends Model
 {
     use HasFactory;
+
+    public function getTrucks()
+    {
+        return $this->hasMany(Truck::class, 'mechanic_id', 'id');
+    }
 }
--- garage/resources/views/mechanic/show.blade.php
+++ garage/resources/views/mechanic/show.blade.php
@@ -9,8 +9,24 @@
                     <h2>Mechanic</h2>
                 </div>
                 <div class="card-body">
-                     <h5>{{$mechanic->name}}</h5>
+                    <div class="mechanic">
+                    <h5>{{$mechanic->name}}</h5>
                     <h5>{{$mechanic->surname}}</h5>
+                    </div>
+                    <ul class="list-group">
+                        @forelse($mechanic->getTrucks as $truck)
+                        <li class="list-group-item">
+                            <div class="trucks-list">
+                                <div class="content">
+                                    <h2><span>plate: </span>{{$truck->plate}}</h2>
+                                    <h4><span>maker: </span>{{$truck->maker}}</h4>
+                                </div>
+                            </div>
+                        </li>
+                        @empty
+                        <li class="list-group-item">No trucks found</li>
+                        @endforelse
+                    </ul>
                 </div>
             </div>
         </div>
