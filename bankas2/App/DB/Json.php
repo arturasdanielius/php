4368ac6a5cf7268d757677db8dc8576ce6bd2370
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-19 11:55:06 +0300

    bankas2, jason create + update start

--- bankas2/App/DB/Json.php
+++ bankas2/App/DB/Json.php
@@ -4,4 +4,44 @@ namespace App\DB;
 
 class Json implements DataBase {
 
+    private $data;
+
+    private function __construct()
+    {
+        if(!file_exists(DIR . 'DB/data.json')) {
+            file_put_contents(DIR . 'DB/data.json', json_encode([])); 
+        }
+        $this->data = json_decode(file_get_contents(DIR . 'DB/data.json'), 1); 
     }
+
+    private function getId() : int
+    {
+        if(!file_exists(DIR . 'DB/data_id.json')) {
+            file_put_contents(DIR . 'DB/data_id.json', json_encode(0)); 
+        }
+        $id = json_decode(file_get_contents(DIR . 'DB/data.json'));
+        $id++;
+        file_put_contents(DIR . 'DB/data_id.json', json_encode($id));
+        return $id;
+    }
+
+    public function __destruct()
+    {
+        file_put_contents(DIR . 'DB/data.json', json_encode([$this->data])); 
+    }
+    //implementacija is DataBase.php public function:
+    public function create(array $userData) : void
+    {
+        $userData['id'] = $this->getId();
+        $this->data[] = $userData;
+    }
+
+    public function update(int $userId, array $userData) : void
+    {
+        foreach ($this->data as &$user){
+           if ($user['id'] == $userId){ 
+                $userData['id'] = $userId; //eilute nebutina - saugumo sumetimais
+           }  
+        }
+    }
+}
