f3275ba5bb8913e942ed0d596c1c85050b2ddbf6
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-22 14:01:05 +0300

    garage sub

--- garage/routes/web.php
+++ garage/routes/web.php
@@ -48,5 +48,6 @@ Route::prefix('breakdown')->name('b_')->group(function () {
     Route::get('/', [B::class, 'index'])->name('index');
 
     Route::get('/trucks-list/{mechanic_id}', [B::class, 'trucksList']);
+    Route::post('/create', [B::class, 'store']);
 
 });
