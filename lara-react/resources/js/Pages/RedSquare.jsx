993ff2c162724ba55856b1cf24aae45cf20c3391
Artūras Danielius <arturas.danielius@gmail.com>
2022-09-27 13:53:48 +0300

    ???

---
+++ lara-react/resources/js/Pages/RedSquare.jsx
@@ -0,0 +1,14 @@
+import PreviousMap from "postcss/lib/previous-map";
+
+function RedSquare(petras) {
+
+    return (
+        <h2 style={{
+            color: petras.color,
+            fontSize: petras.size + 'px'
+        }}>CRIMSON</h2>
+    );
+
+}
+
+export default RedSquare;
--- lara-react/routes/web.php
+++ lara-react/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::get('/red-square', function () {
+    return Inertia::render('RedSquare', [
+        'color' => 'crimson',
+        'size' => '78'
+    ]);
+});
+
+require __DIR__.'/auth.php';
+
