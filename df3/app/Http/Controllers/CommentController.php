14ca5fab959cc48d5c6f8683b1acd0e68b43d080
Artūras Danielius <arturas.danielius@gmail.com>
2022-10-12 12:13:24 +0300

    df3 start many to many

---
+++ df3/app/Http/Controllers/CommentController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+
+    public function index()
+    {
+        return view('comment.index', [
+            'movies' => Movie::orderBy('updated_at', 'desc')->paginate(5),
+           ]);
+    }
+
+    
+    public function destroy(Comment $comment)
+    {
+        $comment->delete();
+
+        return redirect()->back();
+    }
+}
