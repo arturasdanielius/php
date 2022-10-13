e0ea1d67d464bb551e066caa5c2cd68a1e4db275
Artūras Danielius <arturas.danielius@gmail.com>
2022-10-13 13:35:55 +0300

    egzas startas

---
+++ egzas/app/Http/Controllers/MovieController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+
+class MovieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       return view('movie.index', [
+        'movies' => Movie::orderBy('updated_at', 'desc')->paginate(5),
+       ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('movie.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request;  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        $request->validate([
+            'title' => 'required|min:3|max:50',
+            'price' => 'required|numeric|min:1|max:100',
+            'photo.*' => 'sometimes|required|mimes:jpg|max:2000'
+        ]);
+        
+        Movie::create([
+            'title' => $request->title,
+            'price' => $request->price,
+        ])->addImages($request->file('photo'));
+
+        return redirect()->route('m_index')->with('ok', 'All Good!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        return view('movie.show', [
+            'movie' => $movie,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Movie $movie)
+    {
+        return view('movie.edit', [
+            'movie' => $movie,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request;  $request
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Movie $movie)
+    {
+        
+        $request->validate([
+            'title' => 'required|min:3|max:50',
+            'price' => 'required|numeric|min:1|max:100',
+            'photo.*' => 'sometimes|required|mimes:jpg|max:2000'
+        ]);
+
+        $movie->update([
+            'title' => $request->title,
+            'price' => $request->price,
+        ]);
+        $movie
+        ->removeImages($request->delete_photo)
+        ->addImages($request->file('photo'));
+
+        return redirect()->route('m_index')->with('ok', 'All Good!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        if ($movie->getPhotos()->count()) {
+            $delIds = $movie->getPhotos()->pluck('id')->all();
+            $movie->removeImages($delIds);
+        }
+        $title = $movie->title;
+        $movie->delete();
+        return redirect()->route('m_index')->with('ok', "$title gone!");
+    }
+}
---
+++ egzas/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+use Faker\Factory as F;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $time = Carbon::now();
+        $faker = F::create('lt_LT');
+        DB::table('users')->insert([
+            'name' => 'Bebras',
+            'email' => 'bebras@gmail.com',
+            'password' => Hash::make('123'),
+            'created_at' => $time,
+            'updated_at' => $time,
+            'role' => 1
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Briedis',
+            'email' => 'briedis@gmail.com',
+            'password' => Hash::make('123'),
+            'created_at' => $time,
+            'updated_at' => $time,
+            'role' => 10
+        ]);
+
+        foreach([
+            'Total Recall 2',
+            'Tom and Tom 3',
+            'Cobra and Robocop 5',
+            'Shark and Cats',
+            'Blue Gnom',
+            'Lara and Tombs'
+        ] as $movie) {
+            DB::table('movies')->insert([
+                'title' => $movie,
+                'price' => rand(100, 1000) / 100,
+                'created_at' => $time->addSeconds(1),
+                'updated_at' => $time
+            ]);
+        }
+
+
+        foreach(range(1, 22) as $_) {
+            DB::table('comments')->insert([
+                'post' => $faker->paragraph(rand(1, 10)),
+                'movie_id' => rand(1, 6),
+                'created_at' => $time->addSeconds(1),
+                'updated_at' => $time
+            ]);
+        }
+
+
+
+    }
+}
