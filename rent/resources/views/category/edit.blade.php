1731f4aa1d3f005c08228d186a76318e4efa609b
Artūras Danielius <arturas.danielius@gmail.com>
2022-11-12 20:50:51 +0200

    rent category(create ir edit pakeista "name" i "title")

--- rent/resources/views/category/edit.blade.php
+++ rent/resources/views/category/edit.blade.php
@@ -11,12 +11,8 @@
                 <div class="card-body">
                     <form action="{{route('m_edit', $category)}}" method="post">
                     <div class="input-group mb-3">
-                        <span class="input-group-text">Name</span>
-                        <input type="text" name="name" class="form-control" value={{old('name', $category->name)}}>
-                    </div>
-                    <div class="input-group mb-3">
-                        <span class="input-group-text">Surname</span>
-                        <input type="text" name="surname" class="form-control" value={{old('surname', $category->surname)}}>
+                        <span class="input-group-text">Title</span>
+                        <input type="text" name="title" class="form-control" value={{old('name', $category->title)}}>
                     </div>
                     @csrf
                     @method('put')
